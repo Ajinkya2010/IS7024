a383eaf0e1b07ab3716b87390e4a03270b12451a
Ajinkya Dalvi <dalviap@mail.uc.edu>
2019-11-10 20:23:40 -0500

    New Repository Created

---
+++ XML & Web Services/ShowMyData/ShowMyData/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using QuickType;
+
+namespace ShowMyData.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            using (var WebClient = new WebClient())
+            {
+                string jsonString = WebClient.DownloadString("https://data.cincinnati-oh.gov/resource/ksej-uzjq.json");
+                var welcome = Welcome.FromJson(jsonString);
+                ViewData["Welcome"] = welcome;
+            }
+        }
+    }
+}
