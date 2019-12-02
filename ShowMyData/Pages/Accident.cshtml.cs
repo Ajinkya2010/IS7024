61413e9c73764c14b8d8ec4b621bc36cd95ea152
raynor94 <raynor94@gmail.com>
2019-12-02 18:40:21 -0500

    Added JSON data from another student's project

---
+++ ShowMyData/Pages/Accident.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Accident_Type;
+using System.Net;
+
+namespace CrimeRecords.Pages
+{
+    public class AccidentModel : PageModel
+    {
+        public ICollection<Accident> AccidentData { get; set; }
+
+        public AccidentModel()
+        {
+            using (var webClient = new WebClient())
+            {
+                String AccidentJson_Data = webClient.DownloadString("https://cincinnatiaccidents.azurewebsites.net/api/neighbourhoods/downtown");
+                AccidentData = Accident.FromJson(AccidentJson_Data);
+            }
+        }
+        public void OnGet()
+        {
+            foreach (Accident accidents in AccidentData)
+            {
+                ViewData["AccidentData"] = AccidentData;
+            }
+
+
+        }
+    }
+}
