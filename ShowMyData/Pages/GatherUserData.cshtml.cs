d40342f96ae340ccb81cbea03951caec64441c83
prashant5192 <prashantbhosale5192@gmail.com>
2019-12-01 22:15:35 -0500

    Added a form and HTML page to to take and display user input. Added link to this form and user data dashboard on Index.cshtml page

---
+++ ShowMyData/Pages/GatherUserData.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CrimeRecords.Pages
+{
+    public class GatherUserDataModel : PageModel
+    {
+        private readonly ILogger<GatherUserDataModel> _logger;
+
+        [BindProperty]
+        public User user { get; set; }
+
+
+        public GatherUserDataModel(ILogger<GatherUserDataModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnPost()
+        {
+            // update the local map.
+            string UserData = user.FirstName + user.LastName + user.EmailID + user.PhoneNo;
+            UserRoster.allUsers.Add(user);
+            Console.WriteLine(UserData);
+        }
+    }
+}
---
+++ ShowMyData/UserRoster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrimeRecords
+{
+    public class UserRoster
+    {
+        static UserRoster()
+        {
+            allUsers = new List<User>();
+        }
+
+        public static IList<User> allUsers { get; set; }
+    }
+}
