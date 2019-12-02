b14a8a69492f7212fd22b1f1bdef79e82994ad81
Ajinkya Dalvi <dalviap@mail.uc.edu>
2019-12-01 22:40:02 -0500

    Initial Commit_Css

---
+++ IS7024-master/ShowMyData/Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using Crime;
+using Drugs;
+
+namespace ShowMyData.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            SearchCompleted = false;
+        }
+        [BindProperty]
+        public string Search { get; set; }
+        public bool SearchCompleted { get; set; }
+        public ICollection<PoliceCrime> policeCrimes { get; set; }
+        public ICollection<DrugCrime> drugCrimes { get; set; }
+        public void OnPost()
+        {
+            using (var WebClient = new WebClient())
+            {
+                string jsonString = WebClient.DownloadString("https://data.cincinnati-oh.gov/resource/ksej-uzjq.json");
+                policeCrimes = PoliceCrime.FromJson(jsonString);
+                policeCrimes = policeCrimes.Where(x => x.Neighborhood.ToLower().Equals(Search.ToLower())).ToArray();
+                ViewData["PoliceCrime"] = policeCrimes;
+
+                string jsonString_1 = WebClient.DownloadString("https://data.cincinnati-oh.gov/resource/7mtn-nnb5.json");
+                drugCrimes = DrugCrime.FromJson(jsonString_1);
+                drugCrimes = drugCrimes.Where(x => x.CommunityCouncilNeighborhood.ToLower().Equals(Search.ToLower())).ToArray();
+                ViewData["DrugCrimes"] = drugCrimes;
+            }
+            SearchCompleted = true;
+
+        }
+    }
+}
---
+++ IS7024-master/ShowMyData/Pages/StudentForm.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace XML_Indiviual_Assignment.Pages
+{
+    public class StudentFormModel : PageModel
+    {
+        [BindProperty]
+        private string Term { get; set; }
+        private IList<string> studentNames = new List<string>();
+        public JsonResult OnGet()
+        {
+            Term = HttpContext.Request.Query["term"];
+
+            AddStudents("Prashant Dalvi");
+            AddStudents("Prashant Bhosale");
+            AddStudents("Shreejith Nair");
+            AddStudents("Tanmay Jog");
+            AddStudents("Rekha Raj");
+            AddStudents("Rekha Mohan");
+            AddStudents("Rekha GandPad");
+            AddStudents("Rekha Spear");
+            AddStudents("Raynor Moras");
+            AddStudents("Raynor Gaikwad");
+
+            return new JsonResult(studentNames);
+        }
+
+        private void AddStudents(string studentName)
+        {
+            string lowername = studentName.ToLower();
+            string lowerterm = Term.ToLower();
+            if (lowername.Contains(lowerterm))
+            {
+                studentNames.Add(studentName);
+            }
+        }
+    }
+}
