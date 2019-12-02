694c1dc1e4d2cb3fb405048f90be9e6147d3b66f
raynor94 <raynor94@gmail.com>
2019-12-02 16:56:09 -0500

    Added controller to display json based on neighbourhood

---
+++ ShowMyData/Data/CrimeRecordsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrimeRecords.Models
+{
+    public class CrimeRecordsContext : DbContext
+    {
+        public CrimeRecordsContext (DbContextOptions<CrimeRecordsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CrimeRecords.Models.Neighbourhood> Neighbourhood { get; set; }
+    }
+}
---
+++ ShowMyData/Models/Neighbourhood.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrimeRecords.Models
+{
+    public class Neighbourhood
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string Neighborhood { get; set; }
+
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string CrimeType { get; set; }
+
+        public string Weapon { get; set; }
+
+
+
+
+    }
+}
